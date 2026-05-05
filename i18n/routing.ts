c2ddb8345b772bfee790a65f624e4e9d4deb0d17
daws11 <dawskutel@gmail.com>
2026-05-05 17:27:48 +0800

    fix: ganti localePrefix ke never (cookie-based), /en/ prefix butuh restruktur [locale] folder

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,5 +3,5 @@ import { defineRouting } from 'next-intl/routing'
 export const routing = defineRouting({
   locales: ['id', 'en'],
   defaultLocale: 'id',
-  localePrefix: 'as-needed',
+  localePrefix: 'never',
 })
